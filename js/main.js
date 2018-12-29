b47649a1e300aec40fa15b6747880f2c2b70372e
Jenna Murphy <jenna.raye.murphy@gmail.com>
2018-12-29 10:57:12 -0800

    styling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 window.addEventListener('keydown', function (e) {
-    console.log(e.keyCode);
     const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
-    if (!audio) return;
-    audio.play();
+    console.log(audio);
 })
