8ebbe6a3f5cd354b582b4343586e35086bd03b41
Jenna Murphy <jenna.raye.murphy@gmail.com>
2018-12-29 11:36:43 -0800

    add functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 window.addEventListener('keydown', function (e) {
-    const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
-    console.log(audio);
+    console.log(e);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+    if (!audio) return;
+    audio.currentTime = 0; //rewind to start
+    audio.play();
 })
